7623235eaa565dd304ff115f033f873e8a9aa416
Dentropy <personinternet@protonmail.com>
2020-10-22 16:55:20 -0400

    Started reaction analysis, messages now accessible from GeneratedAnalyitcs object instances

--- README.md
+++ README.md
@@ -6,11 +6,14 @@
 
 * [Anaconda data scince tool box](https://www.anaconda.com/products/individual)
   * Packages used include sqlalchemy, matplotlib, and a jupyter notebook
+* Additional pip packages
+  * URLExtract
 * [Keybase](https://keybase.io/download)
 
 ### Run the export tool
 
 ``` bash
+pip3 install URLExtract
 python3 create_export.py
 ```
 If you want to change the keybase team, export json file, or sqlite json file you can do so in the last two lines of the create_export.py file
@@ -26,11 +29,42 @@ jupyter notebook
 Your browser should open with a listing of the files associated with this project. Open generate_analyitics.py and have fun.
 
 
+## GeneratedAnalyitcs Object
+
+* GeneratedAnalyitcs.messages
+  * {{message.id}}
+    * team
+    * from_user
+    * topic
+    * txt_body
+* GeneratedAnalyitcs.user_list
+* GeneratedAnalyitcs.topic_list
+* GeneratedAnalyitcs.characters_per_user
+  * users_list
+  * characters_list
+* GeneratedAnalyitcs.characters_per_topic
+  * topics_list
+  * characters_list
+* GeneratedAnalyitcs.messages_per_user
+  * users_list
+  * messages_list
+* GeneratedAnalyitcs.messages_per_topic
+  * topics_list
+  * messages_list
+* GeneratedAnalyitcs.num_users_per_topic
+  * users_list
+  * topics_list
+* GeneratedAnalyitcs.reaction_per_message
+  * ordered_mesage_id
+  * num_reaction
+* GeneratedAnalyitcs.reaction_sent_per_user
+  * ordered_user
+  * user_to_reaction
+
 ## TODO
 
-* Add Word Count to Database Schema and calculate it during export
-* Plan how to import other message types such as Join, Edit, and Delete to the Database and edit create_export.py accordingly
-* Modify the generate_analyitcs.py script so the result of functions can feed into matplotlib directly
+* Finish the last couple methods in generate_analytics and then output their data in one or both of the jupyter notebooks
+* Import other message types such as Join, Edit, and Delete to the Database
 * Generate as many graphs as resonable using generate_graphs.ipynb
 * Write a script to generate all graphs and export them as images
 
@@ -48,16 +82,20 @@ Your browser should open with a listing of the files associated with this projec
   * Message per Topic
 * Total number of users per
   * Topic
------ Above is completed
+* Most Reactions per
+  * Message
+  * User
 * Total number of people interacting in each channel
+----- Above is completed
+* reaction_poplarity_per_topic
+* user_sent_most_reactions
+* user_recieved_most_reactions
+* reaction_type_popularity_per_user
 * Graph of activity over time
   * Total
   * Channel
   * User
   * User in Channel
-* Most emojis per
-  * Message
-  * Channel, adjusted for total messages
 * Which users are most replied too
 * Average message length per
   * Channel
--- generate_analyitcs.py
+++ generate_analyitcs.py
@@ -1,23 +1,3 @@
-'''
-* GeneratedAnalyitcs Object
-* GeneratedAnalyitcs.user_list
-* GeneratedAnalyitcs.topic_list
-* GeneratedAnalyitcs.characters_per_user
-  * users_list
-  * characters_list
-  * users
-      * {{users name}}
-        * num_characters
-* GeneratedAnalyitcs.characters_per_topic
-  * topics_list
-  * characters_list
-  * topic
-    * {{topic name}}
-      * num_characters
-* GeneratedAnalyitcs.messages_per_user
-* GeneratedAnalyitcs.messages_per_topic
-* GeneratedAnalyitcs.num_users_per_topic
-'''
 from database import DB, Messages
 from sqlalchemy import func
 from sqlalchemy import distinct
@@ -25,6 +5,7 @@ from sqlalchemy import distinct
 class GeneratedAnalyitcs():
     def __init__(self, db_url):
         self.db = DB(db_url)
+        self.get_messages()
         self.get_list_all_users()
         self.get_list_all_topics()
         self.get_characters_per_user()
@@ -32,7 +13,21 @@ class GeneratedAnalyitcs():
         self.get_messages_per_user()
         self.get_messages_per_topic()
         self.get_number_users_per_topic()
+        self.get_reaction_per_message()
+        self.get_reaction_sent_per_user()
+        self.get_reaction_type_popularity()
         
+
+    def get_messages(self):
+        text_messages = self.db.session.query(Messages).filter(Messages.msg_type == "text")
+        self.messages = {}
+        for message in text_messages:
+            self.messages[message.id] = {}
+            self.messages[message.id]["team"] = message.team
+            self.messages[message.id]["from_user"] = message.from_user
+            self.messages[message.id]["topic"] = message.topic
+            self.messages[message.id]["txt_body"] = message.txt_body
+            
     def get_list_all_users(self):
         individual_users = self.db.session.query(distinct(Messages.from_user))
         self.user_list = []
@@ -55,11 +50,10 @@ class GeneratedAnalyitcs():
             for message in user_messages:
                 messages_per_user[user] += len(message.txt_body)
         list_of_users = sorted(messages_per_user, key = messages_per_user.get, reverse=True)
-        self.characters_per_user = {"users_list":[], "characters_list":[], "users":{}}
+        self.characters_per_user = {"users_list":[], "characters_list":[]}
         for item in list_of_users:
             self.characters_per_user["users_list"].append(item)
             self.characters_per_user["characters_list"].append(messages_per_user[item])
-            self.characters_per_user["users"][item] = messages_per_user[item]
         return self.characters_per_user
 
     def get_characters_per_topic(self):
@@ -70,12 +64,10 @@ class GeneratedAnalyitcs():
             for message in topic_messages:
                 messages_per_topic[topic] += len(message.txt_body)
         list_of_users = sorted(messages_per_topic, key = messages_per_topic.get, reverse=True)
-        print(list_of_users)
-        self.characters_per_topic = {"topics_list":[], "characters_list":[], "topics":{}}
+        self.characters_per_topic = {"topics_list":[], "characters_list":[]}
         for item in list_of_users:
             self.characters_per_topic["topics_list"].append(item)
             self.characters_per_topic["characters_list"].append(messages_per_topic[item])
-            self.characters_per_topic["topics"][item] = messages_per_topic[item]
         return self.characters_per_topic
 
     def get_messages_per_user(self):
@@ -85,9 +77,10 @@ class GeneratedAnalyitcs():
             user_messages = self.db.session.query(Messages).filter(Messages.txt_body != None).filter_by(from_user=user)
             messages_per_user[user] = user_messages.count()
         list_of_users = sorted(messages_per_user, key = messages_per_user.get, reverse=True)
-        self.messages_per_user = []
+        self.messages_per_user = {"users_list":[], "messages_list":[]}
         for item in list_of_users:
-            self.messages_per_user.append([item, messages_per_user[item]])
+            self.messages_per_user["users_list"].append(item)
+            self.messages_per_user["messages_list"].append(messages_per_user[item])
         return self.messages_per_user
 
     def get_messages_per_topic(self):
@@ -97,10 +90,11 @@ class GeneratedAnalyitcs():
             topic_messages = self.db.session.query(Messages).filter(Messages.txt_body != None).filter_by(topic=topic)
             messages_per_topic[topic] = topic_messages.count()
         list_of_users = sorted(messages_per_topic, key = messages_per_topic.get, reverse=True)
-        self.messages_per_topic = []
+        self.messages_per_topic = {"topics_list":[], "messages_list":[]}
         for item in list_of_users:
-            self.messages_per_topic.append([item, messages_per_topic[item]])
-        return self.messages_per_topic
+            self.messages_per_topic["topics_list"].append(item)
+            self.messages_per_topic["messages_list"].append(messages_per_topic[item])
+        return self.messages_per_user
 
     def get_number_users_per_topic(self):
         messages_per_topic = {}
@@ -108,9 +102,61 @@ class GeneratedAnalyitcs():
             topic_messages = self.db.session.query(distinct(Messages.from_user)).filter(Messages.txt_body != None).filter_by(topic=topic)
             messages_per_topic[topic] = topic_messages.count()
         list_of_users = sorted(messages_per_topic, key = messages_per_topic.get, reverse=True)
-        self.num_users_per_topic = []
+        self.number_users_per_topic = {"users_list":[], "topics_list":[]}
         for item in list_of_users:
-            self.num_users_per_topic.append([item, messages_per_topic[item]])
-        return self.num_users_per_topic
+            self.number_users_per_topic["users_list"].append(item)
+            self.number_users_per_topic["topics_list"].append(messages_per_topic[item])
+        return self.number_users_per_topic
 
 
+    def get_reaction_per_message(self):
+        messages = {}
+        topic_messages = self.db.session.query(Messages).filter(Messages.msg_type == "reaction")
+        for message in topic_messages:
+            if message.reaction_reference not in messages:
+                messages[message.reaction_reference] = 1
+            else:
+                messages[message.reaction_reference] += 1
+        self.reaction_per_message = {
+                                     "ordered_mesage_id":sorted(messages, key = messages.get, reverse=True), 
+                                     "num_reaction" : messages
+                                    }
+        
+    def get_reaction_sent_per_user(self):
+        users = self.db.session.query(distinct(Messages.from_user)).filter(Messages.msg_type == "reaction")
+        user_to_reaction = {}
+        for user in users:
+            user_to_reaction[user[0]] = self.db.session.query(Messages).filter(Messages.msg_type == "reaction").filter(Messages.from_user == user[0]).count()
+        self.reaction_sent_per_user = {
+                                     "ordered_user":sorted(user_to_reaction, key = user_to_reaction.get, reverse=True), 
+                                     "user_to_reaction" : user_to_reaction
+                                    }
+
+    def get_reaction_type_popularity(self):
+        reaction_messages = self.db.session.query(Messages).filter(Messages.msg_type == "reaction")
+        self.reaction_popularity_map = {"reactions":{}}
+        for reaction in reaction_messages:
+            if reaction.reaction_body not in self.reaction_popularity_map["reactions"]:
+                self.reaction_popularity_map["reactions"][reaction.reaction_body] = 1
+            else:
+                self.reaction_popularity_map["reactions"][reaction.reaction_body] += 1
+        self.reaction_popularity_map["sorted"] = sorted(self.reaction_popularity_map["reactions"], key = self.reaction_popularity_map["reactions"].get, reverse=True), 
+
+    def get_reaction_poplarity_per_topic(self):
+        pass
+
+    def get_all_user_message_id(self):
+        pass
+
+    def get_user_sent_most_reactions(self):
+        # Need get_all_user_message_id
+        pass
+
+    def get_user_recieved_most_reactions(self):
+        # Need get_all_user_message_id
+        pass
+
+
+    def get_reaction_type_popularity_per_user(self):
+        # Need get_all_user_message_id
+        pass
